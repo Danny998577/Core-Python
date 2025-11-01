1a15add63805d90441a9e50b7998f39f501b2694
Danny998577 <168345816+Danny998577@users.noreply.github.com>
2025-11-01 20:59:53 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,33 @@
+"""
+f in print statement is used for f-string (formatted string)
+#variables= A container for a value (String, integer, float , boolean)
+
+
+
+"""
+#String 
+#in a String the statement always write in double or single quotes;
+first_name="Danny"
+email="danny@gmail.com"
+print(f"Hello {first_name}")
+print(f"Mail ID:-{email}")
+
+#integers
+Age=23
+weight=65
+number=27
+print(f"My Age {Age} Years old")
+print(f"My is Weight is {weight}")
+print(f"My fovt Number is {number}")
+
+#Float 
+height=5.6
+print(f"My Height is: {height}")
+
+
+#Boolean
+if_online=False
+if if_online:
+    print("Danny is online")
+else:
+    print("Danny is offline")
