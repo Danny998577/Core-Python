0ebbfde11f9975b888f8a7fa9dcbb09b776ef577
Danny998577 <168345816+Danny998577@users.noreply.github.com>
2025-11-01 21:25:56 +0530

    Add files via upload

---
+++ TypeCasting.py
@@ -0,0 +1,16 @@
+"""
+TypeCasing = The process of coverting a variable onr data type to another 
+str(),int(),float(),boolean(),
+
+
+"""
+name=""
+age=23
+height=5.4
+online=True
+#age=float(age)
+#height=int(height)
+#age=str(age)
+#age+="1"
+name=bool(name)
+print(name)
