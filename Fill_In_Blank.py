0e29b78be9840020d7ed8cacb33bcef7c2383fd9
Danny998577 <168345816+Danny998577@users.noreply.github.com>
2025-11-02 15:23:29 +0530

    midlib game

---
+++ Fill_In_Blank.py
@@ -0,0 +1,28 @@
+print("best mobile in 2025 _____")
+a="SamsungS25Ultra"
+b="Iphone 17"
+c="Vivo x300pro"
+d="Google 9pro"
+print(f"a. {a}, b. {b} , c. {c} , d. {d}")
+select=input("choose the option :")   #b
+correct_option = "a" #right =a
+#option = "a"   
+#print(id(option))
+#print(id(correct_option))
+if select == correct_option:   #a==b    
+    #print(f"{select} is the correct option")
+    print(f"{a} is the correct option")
+elif select == "b": #b==b
+      print(f"{b} is the Wrong option") #print
+elif select == "c": 
+      print(f"{c} is the Wrong option")
+else:
+     print(f"{d} is the Wrong option")
+
+
+
+
+
+
+
+
